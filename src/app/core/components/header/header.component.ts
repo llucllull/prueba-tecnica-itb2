3954abea28178a145cab69e7fee922634fb69d54
Lluc Llull <llull99.80@gmail.com>
2024-12-18 21:49:55 +0100

    header + search bar

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {SearchBarComponent} from "../search-bar/search-bar.component";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    SearchBarComponent
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
