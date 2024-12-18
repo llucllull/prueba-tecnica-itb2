fc0671a481a04b577503d6144edb11f3ae14be81
Lluc Llull <llull99.80@gmail.com>
2024-12-18 21:49:10 +0100

    change api

--- src/app/core/services/activity.service.ts
+++ src/app/core/services/activity.service.ts
@@ -6,17 +6,12 @@ import {Observable} from "rxjs";
   providedIn: 'root'
 })
 export class ActivityService {
-  private apiUrl = 'https://api.hub.touringmalaga.aititubi.es/activities';
+  private apiUrl = 'https://api.tour2b.hub2b.travel/activities';
 
   constructor(private http: HttpClient) { }
 
   getActivitiesData(): Observable<any> {
     const url = `${this.apiUrl}`;
-    return this.http.get(url);
-  }
-
-  getActivityData(id: number): Observable<any> {
-    const url = `${this.apiUrl}/${id}`;
     return this.http.get(url);
   }
 }
